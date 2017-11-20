4c6d1169edfe050b2a7ac7bc2ade9fea21f0c4db
Scott Sinclair <scottysinclair@gmail.com>
2017-11-20 13:02:35 +0100

    empty project.

---
+++ src/main/java/scott/classicspec/ClassicSpec.java
@@ -0,0 +1,12 @@
+package scott.classicspec;
+
+import scott.barleydb.build.specification.staticspec.CommonDefaultsPlatformSpec;
+
+public class ClassicSpec extends CommonDefaultsPlatformSpec {
+
+   public ClassicSpec() {
+    super("scott.classicspec");
+  }
+
+
+}
